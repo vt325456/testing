d3374eede3d8d3c6e01bb38c581771957db73774
vt325456 <vt325@njit.edu>
2024-09-26 20:40:25 -0400

    corrected the calculator test file

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,5 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import add, subtract,multiply,divide
 
 def test_addition():
     '''Test that addition function works '''    
