dd2d9f6436a222563dd3b229b7f0f4b54fa0ecf4
vt325456 <vt325@njit.edu>
2024-09-26 21:39:44 -0400

    Decimal changes

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,23 +1,24 @@
 from calculator.operations import add, subtract, divide, multiply
 from calculator.Calculation import Calculation
+from decimal import Decimal
 
 class Calculator:
     @staticmethod
-    def add(a, b):
+    def add(a: Decimal, b: Decimal) -> Decimal:
         calculation = Calculation(a ,b, add)
         return calculation.get_Output()
     
     @staticmethod
-    def subtract(a, b):
+    def subtract(a: Decimal, b: Decimal) -> Decimal:
         calculation = Calculation(a ,b, subtract)
         return calculation.get_Output()
 
     @staticmethod
-    def multiply(a, b):
+    def multiply(a: Decimal, b: Decimal) -> Decimal:
         calculation = Calculation(a ,b, multiply)
         return calculation.get_Output()
     
     @staticmethod
-    def divide(a, b):
+    def divide(a: Decimal, b: Decimal) -> Decimal:
         calculation = Calculation(a, b, divide)
         return calculation.get_Output()
